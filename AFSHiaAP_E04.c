143bc5bbff5deff916ee4579cf0e2a9a8d76220b
Patrick Sungkharisma <36809780+ArisatoX@users.noreply.github.com>
2019-04-24 19:30:52 +0700

    nomor 1 masih separoh jalannn

---
+++ AFSHiaAP_E04.c
@@ -0,0 +1,150 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+
+static const char *dirpath = "/home/arisatox/Downloads";
+char cipher[] = "qE1~ YMUR2\"`hNIdPzi%^t@(Ao:=CQ,nx4S[7mHFye#aT6+v)DfKL$r?bkOGB>}!9_wV']jcp5JZ&Xl|\\8s;g<{3.u*W-0";
+
+char encrypt(char *x)
+{
+		int ind, i;
+		char *ptr;
+
+		for (i = 0; i < strlen(x); i++)
+		{
+				ptr = strchr(cipher, x[i]);
+
+				if(ptr) 
+				{
+						ind = ptr - cipher;
+						x[i] = cipher[(ind + 17) % strlen(cipher)];
+				}
+		}
+
+		return *x;
+}
+
+char decrypt(char *y)
+{
+		int ind, i;
+		char *ptr;
+
+		for (i = 0; i < strlen(y); i++)
+		{
+				ptr = strchr(cipher, y[i]);
+
+				if(ptr) 
+				{
+						ind = ptr - cipher - 17;
+						if (ind < 0)
+						{
+								ind = ind + strlen(cipher);
+						}
+						y[i] = cipher[ind];
+				}
+		}
+
+		return *y;
+}
+
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+  int res;
+	char fpath[1000], temp[1000];
+
+	strcpy(temp, path);
+
+	decrypt(temp);
+	
+	sprintf(fpath,"%s%s",dirpath,temp);
+
+	res = lstat(fpath, stbuf);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		       off_t offset, struct fuse_file_info *fi)
+{
+  char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(fpath);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+
+		encrypt(de->d_name);
+
+		res = (filler(buf, de->d_name, &st, 0));
+			if(res!=0) break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+  char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+  int fd = 0 ;
+
+	(void) fi;
+	fd = open(fpath, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static struct fuse_operations xmp_oper = {
+	.getattr	= xmp_getattr,
+	.readdir	= xmp_readdir,
+	.read		= xmp_read,
+};
+
+
+int main(int argc, char *argv[])
+{
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
+}
